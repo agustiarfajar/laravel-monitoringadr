beb906190badce8a29bfcb6206de81db167d975a
nurulkafi <nurulkafi18@gmail.com>
2023-08-15 14:23:59 +0700

    otentikasi

---
+++ resources/views/admin/403.blade.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <title>403 Forbidden</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- jquery -->
+    <script src="https://code.jquery.com/jquery-3.7.0.min.js"
+        integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
+    <!-- <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.0/dist/jquery.slim.min.js"></script> -->
+    <!-- Favicons -->
+    <link href="{{ asset('NiceAdmin/assets/img/apple-touch-icon.png') }}" rel="apple-touch-icon">
+    <link href="{{ asset('NiceAdmin/assets/img/logo.jpg') }}" rel="icon">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.gstatic.com" rel="preconnect">
+    <link
+        href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+        rel="stylesheet">
+
+    <!-- Vendor CSS Files -->
+    <link href="{{ asset('NiceAdmin/assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous"> -->
+
+    <link href="{{ asset('NiceAdmin/assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('NiceAdmin/assets/vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('NiceAdmin/assets/vendor/quill/quill.snow.css') }}" rel="stylesheet">
+    <link href="{{ asset('NiceAdmin/assets/vendor/quill/quill.bubble.css') }}" rel="stylesheet">
+    <link href="{{ asset('NiceAdmin/assets/vendor/remixicon/remixicon.css') }}" rel="stylesheet">
+    <link href="{{ asset('NiceAdmin/assets/vendor/simple-datatables/style.css') }}" rel="stylesheet">
+
+    <!-- Template Main CSS File -->
+    <link href="{{ asset('NiceAdmin/assets/css/style.css') }}" rel="stylesheet">
+    <!-- Sweetalert -->
+    <link rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/11.6.16/sweetalert2.min.css">
+
+    <!-- Select2 -->
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" />
+    <link rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/select2-bootstrap-5-theme@1.3.0/dist/select2-bootstrap-5-theme.min.css" />
+    <!-- =======================================================
+  * Template Name : NiceAdmin
+  * Updated: May 30 2023 with Bootstrap v5.3.0
+  * Template URL: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+
+    <!-- ======= Main ======= -->
+    <main>
+        <div class="container">
+
+            <section class="section error-404 min-vh-100 d-flex flex-column align-items-center justify-content-center">
+                {{-- <h1>403</h1> --}}
+                <img src="{{ asset('NiceAdmin/assets/img/403.svg') }}" class="img-fluid py-5" alt="Page Not Found">
+                <h2>Maaf, Anda tidak memiliki izin untuk mengakses halaman ini.</h2>
+                <a class="btn" href="{{ url('/') }}">Back to home</a>
+            </section>
+
+        </div>
+    </main>
+
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
+            class="bi bi-arrow-up-short"></i></a>
+
+    <!-- Vendor JS Files -->
+    <script src="{{ asset('NiceAdmin/assets/vendor/apexcharts/apexcharts.min.js') }}"></script>
+    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous">
+    </script> -->
+    <script src="{{ asset('NiceAdmin/assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('NiceAdmin/assets/vendor/chart.js/chart.umd.js') }}"></script>
+    <script src="{{ asset('NiceAdmin/assets/vendor/echarts/echarts.min.js') }}"></script>
+    <script src="{{ asset('NiceAdmin/assets/vendor/quill/quill.min.js') }}"></script>
+    <script src="{{ asset('NiceAdmin/assets/vendor/simple-datatables/simple-datatables.js') }}"></script>
+    <script src="{{ asset('NiceAdmin/assets/vendor/tinymce/tinymce.min.js') }}"></script>
+    <script src="{{ asset('NiceAdmin/assets/vendor/php-email-form/validate.js') }}"></script>
+    <link href="https://cdn.datatables.net/v/bs5/dt-1.13.5/datatables.min.css" rel="stylesheet" />
+
+    <!-- Template Main JS File -->
+    <script src="{{ asset('NiceAdmin/assets/js/main.js') }}"></script>
+    <!--script>
+    function nextForm() {
+      document.getElementById('multi-form').style.display = 'none';
+      document.getElementById('second-form').style.display = 'block';
+    }
+
+    function prevForm() {
+      document.getElementById('multi-form').style.display = 'block';
+      document.getElementById('second-form').style.display = 'none';
+    }
+  </script-->
+    <!-- Sweetalert -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/11.6.16/sweetalert2.min.js"></script>
+
+    <!-- <script src="https://cdn.datatables.net/v/bs5/dt-1.13.5/datatables.min.js"></script> -->
+    <!-- Select2 -->
+
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -53,6 +53,9 @@ Route::get('/', function () {
     }
     // return redirect('/admin-dashboard');
 })->middleware('auth');
+Route::get('/no-access',function(){
+    return view('admin.403');
+});
 Route::get('/login', [HomeController::class, 'login'])->name('login');
 Route::post('/logout', [HomeController::class, 'logout'])->name('logout');
 Route::post('/prosesLogin', [HomeController::class, 'prosesLogin']);
@@ -65,9 +68,88 @@ Route::get('/userfaq', [HomeController::class, 'userfaq']);
 
 Route::middleware(['auth'])->group(function () {
     // Rute-rute yang memerlukan otentikasi
+    Route::group(['middleware' => 'check.permission:dashboard'], function () {
+        Route::get('/admin-dashboard', [AdminController::class, 'dashboard']);
+        // Dashboard periode
+        Route::post('/sisa-barang-ho-update/periode', [AdminController::class, 'update_sisa_barang_ho_periode']);
+        Route::post('/barang-masuk-update/periode', [AdminController::class, 'update_barang_masuk']);
+        Route::post('/barang-keluar-update/periode', [AdminController::class, 'update_barang_keluar']);
+        Route::post('/surat-jalan-update/periode', [AdminController::class, 'update_surat_jalan_periode']);
+        Route::post('/barang-aging-update/periode', [AdminController::class, 'update_barang_aging_periode']);
+        Route::post('/belum-kirim-pemasok-update/periode', [AdminController::class, 'update_belum_kirim_pemasok_periode']);
+        Route::post('/belum-terima-site-update/periode', [AdminController::class, 'update_belum_terima_site_periode']);
+        Route::post('/batal-proses-update/periode', [AdminController::class, 'update_batal_proses_periode']);
+        Route::post('/chart-update/periode', [AdminController::class, 'update_chart_periode']);
+        Route::post('/chart-pengiriman-update/periode', [AdminController::class, 'update_chart_pengiriman_periode']);
 
-    Route::get('/admin-dashboard', [AdminController::class, 'dashboard']);
-    Route::get('/adminstatus', [AdminController::class, 'adminstatus']);
+        Route::get('/print/{id}', [PrintController::class, 'print']);
+        Route::get('/printho/{id}', [PrintController::class, 'print_ho']);
+
+    });
+    Route::group(['middleware' => 'check.permission:perusahaan'], function () {
+        // Perusahaan
+        Route::get('/perusahaan', [AdminController::class, 'view_perusahaan']);
+        Route::post('/perusahaan/save', [AdminController::class, 'save_perusahaan']);
+        Route::post('/perusahaan/update/{id}', [AdminController::class, 'update_perusahaan']);
+        Route::get('/perusahaan/delete/{id}', [AdminController::class, 'delete_perusahaan']);
+    });
+    Route::group(['middleware' => 'check.permission:ekspedisi'], function () {
+        /// Ekspedisi
+        Route::get('/ekspedisi', [AdminController::class, 'view_ekspedisi']);
+        Route::post('/ekspedisi/save', [AdminController::class, 'save_ekspedisi']);
+        Route::post('/ekspedisi/update/{id}', [AdminController::class, 'update_ekspedisi']);
+        Route::get('/ekspedisi/delete/{id}', [AdminController::class, 'delete_ekspedisi']);
+    });
+    Route::group(['middleware' => 'check.permission:user'], function () {
+        //User
+        Route::get('/user-access', [UserController::class, 'view_user']);
+        Route::get('/getUserDetailJson/{id}', [UserController::class, 'getUserDetailJson']);
+        Route::post('/createUsers', [UserController::class, 'create_user']);
+        Route::post('/updateUsers/{id}', [UserController::class, 'update_user']);
+        Route::delete('/deleteUsers/{id}', [UserController::class, 'delete_user']);
+    });
+    Route::group(['middleware' => 'check.permission:role'], function () {
+        //Role
+        Route::get('/role-access', [RoleController::class, 'view_role']);
+        Route::post('/saveRole', [RoleController::class, 'save_role']);
+        Route::post('/updateRole/{id}', [RoleController::class, 'update_role']);
+        Route::delete('/deleteRole/{id}', [RoleController::class, 'delete_role']);
+        Route::get('/checkNameRole/{name}', [RoleController::class, 'checkNameRole']);
+        Route::get('/getRole/json/{id}', [RoleController::class, 'getRoleByJson']);
+        Route::get('/getCountUser/json/{id}', [RoleController::class, 'getCountUser']);
+    });
+    Route::group(['middleware' => 'check.permission:barangHO'], function () {
+        Route::get('/tambahitem', [AdminController::class, 'additem']);
+        Route::post('/simpan-item', [AdminController::class, 'simpan_item']);
+        Route::get('/edit-item/{id}', [AdminController::class, 'edit_item']);
+        Route::post('/update-item/{id}', [AdminController::class, 'update_item']);
+        Route::get('/delete-item/{id}', [AdminController::class, 'delete_item']);
+        Route::get('/detail/pengiriman-site/{id}', [AdminController::class, 'detail_pengiriman_site']);
+    });
+    Route::group(['middleware' => 'check.permission:pengiriman'], function () {
+        Route::get('/adminstatus', [AdminController::class, 'adminstatus']);
+        Route::get('/tambah-pengiriman-site', [FormController::class, 'index'])->name('form.index');
+        Route::post('/simpan-pengiriman-site', [FormController::class, 'save_barang']);
+
+        Route::get('/tambah-pengiriman', [FormController::class, 'index2'])->name('form.index2');
+        Route::post('/simpan-pengiriman-ho', [FormController::class, 'simpan_pengiriman_ho']);
+        Route::post('/get-item', [FormController::class, 'get_item']);
+        Route::get('/detail/pengiriman-ho/{id}',
+            [AdminController::class, 'detail_pengiriman_ho']
+        );
+        Route::get('/reset-form', [FormController::class, 'reset'])->name('form.reset');
+        Route::get('/form', [FormController::class, 'form'])->name('form.first');
+    });
+
+    Route::group(['middleware' => 'check.permission:laporan'], function () {
+        // Laporan
+        Route::get('/laporan', [AdminController::class, 'laporan']);
+        Route::get('/export-laporan-kpi', [AdminController::class, 'exportDataToCsv']);
+    });
+
+
+
+
     Route::post('/filter-pengiriman', [AdminController::class, 'filter_pengiriman']);
     Route::get('/adminfaq', [AdminController::class, 'adminfaq']);
 
@@ -77,30 +159,10 @@ Route::middleware(['auth'])->group(function () {
 
     //Route::get('/tambah-pengiriman', [AdminController::class, 'addpengiriman']);
 
-    Route::get('/tambah-pengiriman-site', [FormController::class, 'index'])->name('form.index');
-    Route::post('/simpan-pengiriman-site', [FormController::class, 'save_barang']);
-
-    Route::get('/tambah-pengiriman', [FormController::class, 'index2'])->name('form.index2');
-    Route::post('/simpan-pengiriman-ho', [FormController::class, 'simpan_pengiriman_ho']);
-    Route::post('/get-item', [FormController::class, 'get_item']);
-    Route::get('/detail/pengiriman-ho/{id}', [AdminController::class, 'detail_pengiriman_ho']);
-    Route::get('/reset-form', [FormController::class, 'reset'])->name('form.reset');
-    Route::get('/form', [FormController::class, 'form'])->name('form.first');
-
     Route::delete('/delete-data/{id}', [FormController::class, 'deleteData'])->name('data.delete');
 
     Route::get('/data', [DataController::class, 'index'])->name('data.index');
 
-    Route::get('/tambahitem', [AdminController::class, 'additem']);
-    Route::post('/simpan-item', [AdminController::class, 'simpan_item']);
-    Route::get('/edit-item/{id}', [AdminController::class, 'edit_item']);
-    Route::post('/update-item/{id}', [AdminController::class, 'update_item']);
-    Route::get('/delete-item/{id}', [AdminController::class, 'delete_item']);
-    Route::get('/detail/pengiriman-site/{id}', [AdminController::class, 'detail_pengiriman_site']);
-
-    // Laporan
-    Route::get('/laporan', [AdminController::class, 'laporan']);
-    Route::get('/export-laporan-kpi', [AdminController::class, 'exportDataToCsv']);
 
     // Update status pengiriman pemasok
     Route::post('/update-status/kirim/{id}', [AdminController::class, 'update_status_kirim']);
@@ -112,46 +174,4 @@ Route::middleware(['auth'])->group(function () {
     Route::post('/update-status-ho/batal/{id}', [AdminController::class, 'update_status_batal_ho']);
     //Route::post('/post', [FormController::class, 'post'])->name('admin.status');
 
-    // Perusahaan
-    Route::get('/perusahaan', [AdminController::class, 'view_perusahaan']);
-    Route::post('/perusahaan/save', [AdminController::class, 'save_perusahaan']);
-    Route::post('/perusahaan/update/{id}', [AdminController::class, 'update_perusahaan']);
-    Route::get('/perusahaan/delete/{id}', [AdminController::class, 'delete_perusahaan']);
-
-    // Ekspedisi
-    Route::get('/ekspedisi', [AdminController::class, 'view_ekspedisi']);
-    Route::post('/ekspedisi/save', [AdminController::class, 'save_ekspedisi']);
-    Route::post('/ekspedisi/update/{id}', [AdminController::class, 'update_ekspedisi']);
-    Route::get('/ekspedisi/delete/{id}', [AdminController::class, 'delete_ekspedisi']);
-
-    // Dashboard periode
-    Route::post('/sisa-barang-ho-update/periode', [AdminController::class, 'update_sisa_barang_ho_periode']);
-    Route::post('/barang-masuk-update/periode', [AdminController::class, 'update_barang_masuk']);
-    Route::post('/barang-keluar-update/periode', [AdminController::class, 'update_barang_keluar']);
-    Route::post('/surat-jalan-update/periode', [AdminController::class, 'update_surat_jalan_periode']);
-    Route::post('/barang-aging-update/periode', [AdminController::class, 'update_barang_aging_periode']);
-    Route::post('/belum-kirim-pemasok-update/periode', [AdminController::class, 'update_belum_kirim_pemasok_periode']);
-    Route::post('/belum-terima-site-update/periode', [AdminController::class, 'update_belum_terima_site_periode']);
-    Route::post('/batal-proses-update/periode', [AdminController::class, 'update_batal_proses_periode']);
-    Route::post('/chart-update/periode', [AdminController::class, 'update_chart_periode']);
-    Route::post('/chart-pengiriman-update/periode', [AdminController::class, 'update_chart_pengiriman_periode']);
-
-    Route::get('/print/{id}', [PrintController::class, 'print']);
-    Route::get('/printho/{id}', [PrintController::class, 'print_ho']);
-
-    //User
-    Route::get('/user-access', [UserController::class, 'view_user']);
-    Route::get('/getUserDetailJson/{id}', [UserController::class, 'getUserDetailJson']);
-    Route::post('/createUsers', [UserController::class, 'create_user']);
-    Route::post('/updateUsers/{id}', [UserController::class, 'update_user']);
-    Route::delete('/deleteUsers/{id}', [UserController::class, 'delete_user']);
-
-    //Role
-    Route::get('/role-access', [RoleController::class, 'view_role']);
-    Route::post('/saveRole', [RoleController::class, 'save_role']);
-    Route::post('/updateRole/{id}', [RoleController::class, 'update_role']);
-    Route::delete('/deleteRole/{id}', [RoleController::class, 'delete_role']);
-    Route::get('/checkNameRole/{name}', [RoleController::class, 'checkNameRole']);
-    Route::get('/getRole/json/{id}', [RoleController::class, 'getRoleByJson']);
-    Route::get('/getCountUser/json/{id}', [RoleController::class, 'getCountUser']);
 });
